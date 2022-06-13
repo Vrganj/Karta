44e6f77146cb0a2d3272cc0149d6234a9e10fe3c
Luka Barbić <luka.barbic0@gmail.com>
2022-06-13 22:01:16 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+group = "me.vrganj"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
---
+++ src/main/java/me/vrganj/karta/Karta.java
@@ -0,0 +1,11 @@
+package me.vrganj.karta;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Karta extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("moo");
+    }
+}
